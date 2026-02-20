edf5ef3ec0123490456852b0d73d3cc8f9c6e0ba
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2026-02-20 15:25:12 +0200

    654654

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold text-orange-400 text-center mt-20">Hello or not hello?</h1>
+      <button className="bg-blue-500 rounded-2xl mt-10 mx-auto block p-2 ">Click me</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/public/login.jsx
@@ -0,0 +1,5 @@
+import { Form } from 'react-router';
+
+export default function Login() {
+  return <Form></Form>;
+}
