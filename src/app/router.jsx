3f78348d5873383013fea6780c743945d2fa25b9
DevMasterX <sokol080288@gmail.com>
2026-02-23 00:20:28 +0200

    3636

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,4 +1,4 @@
-import LoginPage from '../routes/public/login';
+import LoginPage from '@/routes/public/login';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
