ff6bffbe6caf1c9385b90a9b3b4981fa7e94c137
DevMasterX <sokol080288@gmail.com>
2026-03-09 23:17:06 +0200

    0000

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import { fetchingError, fetchingInProgress, fetchingSuccess } from './booksSlice';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
 axios.defaults.baseURL = 'http://localhost:3001/';
 
-export const fetchBooks = () => async dispatch => {
-  try {
-    dispatch(fetchingInProgress());
-    const response = await axios.get('books');
-    dispatch(fetchingSuccess(response.data));
-  } catch (error) {
-    dispatch(fetchingError(error.message));
-  }
-};
+// export const fetchBooks = () => async dispatch => {
+//   try {
+//     dispatch(fetchingInProgress());
+//     const response = await axios.get('books');
+//     dispatch(fetchingSuccess(response.data));
+//   } catch (error) {
+//     dispatch(fetchingError(error.message));
+//   }
+// };
+
+// First, create the thunk
+const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const books = await axios.get('books');
+  return books;
+});
