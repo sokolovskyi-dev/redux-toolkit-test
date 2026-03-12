f897c6ca1a042e626ef8b13934f846b131894799
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2026-03-12 10:18:38 +0200

    3242342

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
