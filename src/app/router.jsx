596c65e909f76cc4c740a7765246aaaa117a7bbd
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2026-03-12 10:42:48 +0200

    23432

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -3,21 +3,24 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { loginAction } from '@/routes/public/login/login.action';
 
-export const router = createBrowserRouter([
-  {
-    path: '/login',
-    element: <LoginPage />,
-    action: loginAction,
-    // lazy: () => import('../routes/public/login'),
-  },
-  {
-    path: '/',
-    lazy: () => import('../routes/protected/layout'),
-    children: [
-      { path: 'dashboard', lazy: () => import('../routes/protected/dashboard') },
-      { path: 'todo', lazy: () => import('../routes/protected/todo') },
-      { path: 'async-tasks', lazy: () => import('../routes/protected/asyncTasks') },
-      { path: 'books', lazy: () => import('../routes/protected/books') },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/login',
+      element: <LoginPage />,
+      action: loginAction,
+      // lazy: () => import('../routes/public/login'),
+    },
+    {
+      path: '/',
+      lazy: () => import('../routes/protected/layout'),
+      children: [
+        { path: 'dashboard', lazy: () => import('../routes/protected/dashboard') },
+        { path: 'todo', lazy: () => import('../routes/protected/todo') },
+        { path: 'async-tasks', lazy: () => import('../routes/protected/asyncTasks') },
+        { path: 'books', lazy: () => import('../routes/protected/books') },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
